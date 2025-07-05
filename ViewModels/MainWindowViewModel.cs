e68128943bc59ba1ce86c36b44db3b72925658b6
Aftamat4ik <aftamat4ik@gmail.com>
2025-07-05 06:03:44 +0500

    Add project files.

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace PDFToImage.ViewModels
+{
+    public partial class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting { get; } = "Welcome to Avalonia!";
+    }
+}
---
+++ ViewModels/ViewModelBase.cs
@@ -0,0 +1,8 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace PDFToImage.ViewModels
+{
+    public class ViewModelBase : ObservableObject
+    {
+    }
+}
